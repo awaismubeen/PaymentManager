01b257de18d47a39b4a14d0302d4366b3ef8de4a
Awais Mubeen <awaismubeen@gmail.com>
2025-08-08 18:08:41 +0500

    issue fixed

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "AMPlaceholder",
+            name: "PaymentManager",
             targets: ["PaymentManager"]
         ),
     ],
